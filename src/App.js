63271f1f51f8c7dc7c79a84210973563460469e8
nbbhimani <nb.bhimani@gmail.com>
2024-07-17 18:20:29 +0530

    Fixed some issues
    Minor changes done in App.js, Navbar.js and TextForm.js

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
       <div className="container my-3">
 
-        <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces, listen text" mode={mode} />
+        <TextForm showAlert={showAlert} heading="Try Text_Utilities - Word Counter, Character Counter, Remove extra spaces, listen text" mode={mode} />
 
       </div>
 
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -40,10 +40,7 @@ export default function TextForm(props) {
 
 
     const handleCopy = () => {
-        let text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Text has been Copied to Clipboard", "success");
     }
 
@@ -66,15 +63,15 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleUpClick}>Convert To UpperCase</button>
             <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleLoClick}>Convert To LowerCase</button>
             <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} type="submit" onClick={speak} >Speak</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} type="submit" onClick={speak} >Listen Text</button>
             <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
 
         <div className={`container my-3 text-${props.mode==='light'?'dark':'light'}`}>
             <h4>Your text summary</h4>
-            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters with spaces</p>
-            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters with spaces</p>
+            <p>{0.008 * text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h4>Your text Preview</h4>
             <p>{text.length>0?text:"Nothing to preview"}</p>
         </div>
