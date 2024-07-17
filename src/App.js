07fbb4c51b7ee9f2ead6fd83f554836c444f0d6d
nbbhimani <nb.bhimani@gmail.com>
2024-07-17 12:01:07 +0530

    Major Modifications in all files
    We done major modifications in all files & removed About component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-// import logo from './logo.svg';
-
 import React, { useState } from 'react'
 
 import './App.css';
-
-// import About from './Components/About';
 
 import Navbar from './Components/Navbar';
 
@@ -13,24 +9,17 @@ import TextForm from './Components/TextForm';
 import Alert from './Components/Alert';
 
 
-// import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";  // This is old one & it is depricated
-
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';    // This is new one in latest version
-
-
-
-
 
 function App() {
 
-  const [mode, setMode] = useState('light');    // This is stating that default mode of the all is light
+  const [mode, setMode] = useState('light');
 
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type      // Here we can give same name also that's why we write type: type here
+      type: type
     })
 
     setTimeout(() => {
@@ -41,66 +30,30 @@ function App() {
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';
+      document.body.style.backgroundColor = 'rgb(20 61 94)';
       showAlert("Dark mode has been enabled", "success");
-      // document.title = "Text_Utilities - Dark Mode"    // This will change the title of the we page Dynamically when Dark Mode enabled
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      // document.title = "Text_Utilities - Light Mode" // This will change the title of the we page Dynamically when light Mode enabled
     }
 
   }
 
   return (
+
     <>
 
-      {/* ---------------- Code Without React Router starts from here for to deploy this app on GitHub ------------------- */}
-
-
-
-      <Navbar titel="Text_Utilities" HomeTexts="Home" AboutTexts="About Us" mode={mode} toggleMode={toggleMode} />   {/* Her we passed props */}
-
-      {/* <Navbar/>    */}
-      {/* When we not pass any Props like above then the React app will take Default Propes from the Navbar.js component */}
+      <Navbar titel="Text_Utilities" HomeTexts="Home" mode={mode} toggleMode={toggleMode} />
 
       <Alert alert={alert} />
 
       <div className="container my-3">
 
-        <TextForm showAlert={showAlert} heading="Enter the texts below to analyze" mode={mode} />
+        <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces, listen text" mode={mode} />
 
       </div>
-
-
-      {/* ---------------- Code Without React Router Ends here for to deploy this app on GitHub ------------------- */}
-
-
-
-
-
-      {/* ---------------- Code With React Router starts from here ------------------- */}
-
-
-      {/* <Router> */}
-        {/* <Navbar titel="Text_Utilities" HomeTexts="Home" AboutTexts="About Us" mode={mode} toggleMode={toggleMode} /> */}
-
-        {/* <Alert alert={alert} /> */}
-
-        {/* <div className="container my-3"> */}
-
-          {/* <Routes> */}
-            {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the texts below to analyze" mode={mode} />} /> */}
-            {/* <Route exact path="/about" element={<About />} /> */}
-            {/* <Route path="/contact" element={<Contact />} /> */}
-          {/* </Routes> */}
-        {/* </div> */}
-      {/* </Router > */}
-
-
-      {/* ---------------- Code With React Router Ends here ------------------- */}
 
 
     </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,38 +1,33 @@
-import React, {useState} from 'react'       // Here we import "useState" to use "state" variable
+import React, {useState} from 'react'
 
 
 export default function TextForm(props) {
     
-    const [text, setText] = useState('Enter text here');        // See below comment # 1
-
-    // text = "sjgldhvrjsndll";        // We can not update state/value of "text" variable when it is declared using "useState" as above
+    const [text, setText] = useState('Enter text here');
 
 
-    const handleOnChange = (event) =>{       // This we used below in textarea element inside the JSX
-        // console.log("On Change");
-        setText(event.target.value);         // Remember this -- We will use this in "handleOnChange" function again & again in future
+    const handleOnChange = (event) =>{
+        setText(event.target.value);
     }
     
 
-
-    const handleUpClick = () =>{        // This we used below in button element inside the JSX
-        // console.log(text);        // This we can see in console section after click on the button
+    const handleUpClick = () =>{
         let newText = text.toUpperCase();
-        setText(newText);       // This will change the state/value of the "text" variable after click on button & we can see it in "textbox" on web page
+        setText(newText);
         props.showAlert("Your text has been converted to uppercase", "success");
     }
 
 
-    const handleLoClick = () =>{        // This we used below in button element inside the JSX
+    const handleLoClick = () =>{
         let newText = text.toLowerCase();
-        setText(newText);       // This will change the state/value of the "text" variable after click on button & we can see it in "textbox" on web page
+        setText(newText);
         props.showAlert("Your text has been converted to lowercase", "success");
     }
 
 
-    const handleClearClick = () =>{        // This we used below in button element inside the JSX
+    const handleClearClick = () =>{
         let newText = "";
-        setText(newText);       // This will change the state/value of the "text" variable after click on button & we can see it in "textbox" on web page
+        setText(newText);
         props.showAlert("Text Cleared ", "success");
     }
 
@@ -48,6 +43,7 @@ export default function TextForm(props) {
         let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Text has been Copied to Clipboard", "success");
     }
 
@@ -65,26 +61,23 @@ export default function TextForm(props) {
         <div>
             <h4 className={`my-3 text-${props.mode==='light'?'dark':'light'}`}> {props.heading}</h4>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" placeholder='Enter text here' style={{backgroundColor: props.mode === 'dark' ? '#c7cccf' : 'white'}}></textarea>
             </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To UpperCase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To LowerCase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-2" type="submit" onClick={speak} >Speak</button>
-            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleUpClick}>Convert To UpperCase</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleLoClick}>Convert To LowerCase</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} type="submit" onClick={speak} >Speak</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-1 my-1" disabled={text.length===0} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
 
         <div className={`container my-3 text-${props.mode==='light'?'dark':'light'}`}>
             <h4>Your text summary</h4>
-            <p>{text.split(" ").length-1} words and {text.length} characters with spaces</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters with spaces</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h4>Your text Preview</h4>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Nothing to preview"}</p>
         </div>
         </>
     )
 }
-
-
-// Comment # 1 ----- Here we add/declare a "state" variable "text" with the function name "setText" of it - Here Initial state of the variable "text" is 'Enter text here' & it will be updated with the use of the function "setText" whenever we will update it & it will update when we type something in the "Text box"  on webpage & click on
